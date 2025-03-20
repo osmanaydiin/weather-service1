f22f2c7bafbba6e63b4490f2f6dc47ef575e9c02
Osman Aydın <osmnaydin1453@gmail.com>
2025-03-20 22:12:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,35 +17,7 @@ if (!GOOGLE_MAPS_API_KEY) {
 // CORS middleware'ini ekleyin
 app.use(cors());
 
-/* // İzin verilen IP adresi
-const allowedIP = '123.456.789.000'; // Buraya izin vermek istediğiniz IP adresini yazın */
-
-/* // IP adresini kontrol eden middleware
-app.use((req, res, next) => {
-    const requestIP = req.ip || req.connection.remoteAddress;
-    if (requestIP === allowedIP) {
-        next(); // İzin verilen IP adresi ise devam et
-    } else {
-        res.status(403).send('Erişim engellendi: Bu IP adresine izin verilmiyor.');
-    }
-}); */
-
-
-// İzin verilen web adresi
-/* const allowedReferer = 'http://localhost:3000/'; // Buraya izin vermek istediğiniz web adresini yazın
-
-
-// Referer başlığını kontrol eden middleware
-app.use((req, res, next) => {
-    const referer = req.get('Referer');
-    if (referer && referer.startsWith(allowedReferer)) {
-        next(); // İzin verilen web adresi ise devam et
-    } else {
-        res.status(403).send('Erişim engellendi: Bu web adresine izin verilmiyor.');
-    }
-});
- */
-const allowedOrigins = ['http://127.0.0.1:3001', 'http://localhost:3001', 'https://localhost:3001'];
+const allowedOrigins = ['https://weather-theta-virid.vercel.app/'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
@@ -81,12 +53,12 @@ app.get('/', async (req, res) => {
     try {
         res.send("hello world");
     } catch (error) {
-        console.error('Google Maps API proxy hatası:', error);
+        console.error('hello world hatası:', error);
         res.status(404).send(`not found`);
     }
 });
 
 // Sunucunun çalıştığını kontrol etmek için
 app.listen(3002, () => {
-    console.log('Server running on port 3001');
+    console.log('Server running on port 3002');
 }); 
